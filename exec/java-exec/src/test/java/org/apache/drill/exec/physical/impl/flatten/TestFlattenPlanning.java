d40772ee7f17a5d24ee64944aa75c8689a9910b1
Alex Barreto <shakamunyi@gmail.com>
2015-10-15 10:28:05 -0700

    2015-10-15:10:27:58

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/flatten/TestFlattenPlanning.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/flatten/TestFlattenPlanning.java
@@ -6,34 +6,25 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.drill.exec.server;
+package org.apache.drill.exec.physical.impl.flatten;
 
-import org.apache.drill.exec.client.DrillClient;
-import org.apache.drill.exec.memory.OutOfMemoryException;
-import org.glassfish.hk2.api.Factory;
+import org.apache.drill.PlanTestBase;
+import org.junit.Test;
 
-public class DrillClientFactory implements Factory<DrillClient> {
-//  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DrillClientFactory.class);
+public class TestFlattenPlanning extends PlanTestBase {
 
-  @Override
-  public void dispose(DrillClient arg0) {
-  }
-
-  @Override
-  public DrillClient provide() {
-    try {
-      return new DrillClient();
-    } catch(OutOfMemoryException e) {
-      throw new RuntimeException(e);
-    }
+  @Test
+  public void testFlattenPlanningAvoidUnnecessaryProject() throws Exception {
+    testPlanSubstrPatterns("select flatten(complex), rownum from cp.`/store/json/test_flatten_mappify2.json`",
+        new String[]{"Project(EXPR$0=[$1], rownum=[$0])"}, new String[]{"Project(EXPR$0=[$0], EXPR$1=[$1], EXPR$3=[$1])"});
   }
 }
